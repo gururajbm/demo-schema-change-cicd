8f263a8ebd6f62673cb597242aa3c3705cb1cd62
gururajbm <guru.bm@gmail.com>
2023-12-19 17:37:59 +0530

    ecom

--- dbscripts/test_schema_change/order/V2.1.1__ecommerce.sql
+++ dbscripts/test_schema_change/order/V2.1.1__ecommerce.sql
@@ -1,17 +1,15 @@
-USE ECOMMERCE;
-
-create table User (
-  id      number  autoincrement start 1 increment 1,
-  name    varchar,
-  address varchar
+create or replace TABLE TEST_SCHEMACHANGE.ECOMMERCE.USER (
+	ID NUMBER(38,0) autoincrement start 1 increment 1 order,
+	NAME VARCHAR(16777216),
+	ADDRESS VARCHAR(16777216)
 );
 
 ALTER TABLE User ADD PRIMARY KEY (id);
 
-CREATE TABLE product 
+create or replace TABLE TEST_SCHEMACHANGE.Product 
 ( 
      id      number autoincrement start 1 increment 1, 
-     NAME    VARCHAR(10), 
+     name    VARCHAR(10), 
      tagline VARCHAR(100),
      PRIMARY KEY (id)
 );
