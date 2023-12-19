806515c63994d37ee93f1c2ca5950d3df6b8d374
gururajbm <guru.bm@gmail.com>
2023-12-19 18:51:24 +0530

    vw

---
+++ dbscripts/test_schema_change/ecommerce/R__cart_vw.sql
@@ -0,0 +1,11 @@
+
+USE SCHEMA ECOMMERCE;
+
+CREATE VIEW user_cart_view AS
+SELECT u.name as username, p.name as product_name
+    FROM TEST_SCHEMACHANGE.ECOMMERCE.cart AS c 
+    JOIN TEST_SCHEMACHANGE.ECOMMERCE.user AS u 
+        ON c.user_id = u.id
+    JOIN TEST_SCHEMACHANGE.ECOMMERCE.product AS p 
+        ON c.product_id = p.id        
+    ORDER BY c.id;
